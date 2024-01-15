4ad82976e54a92e81dccde7c1d2e642b2e0e61b7
PhamQuocHungK15 <phamquochung12082001@gmail.com>
2024-01-15 21:43:59 +0700

    add controller
    abc

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WebApplication1.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "1", "2", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "1", "2", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Hung"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
