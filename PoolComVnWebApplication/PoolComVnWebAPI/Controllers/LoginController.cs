c5fabf10ec7cc5820886e60088b6aebdcb22d500
dinhthenam <111273152+dinhthenam@users.noreply.github.com>
2024-01-24 21:52:17 +0700

    jwt token ( still bug)

---
+++ PoolComVnWebApplication/PoolComVnWebAPI/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using BusinessObject.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Principal;
+using System.Text;
+
+namespace PoolComVnWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        private Account AuthenticateUser(Account account)
+        {
+            return account;
+        }
+        private string GenerateToken(Account account)
+        {
+            var securitykey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credential = new SigningCredentials(securitykey, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(_configuration["Jwt:Issuer"], _configuration["Jwt:Audience"], null,
+                expires:DateTime.Now.AddMinutes(1),signingCredentials:credential
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login(Account account)
+        {
+            IActionResult response = Unauthorized();
+            var account_ = AuthenticateUser(account);
+            if (account != null)
+            {
+                var token = GenerateToken(account_);
+                response = Ok(new { token = token });
+            }
+            return response;
+        }
+    }
+}
--- PoolComVnWebApplication/PoolComVnWebAPI/Program.cs
+++ PoolComVnWebApplication/PoolComVnWebAPI/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +10,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(option =>
+    {
+        option.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
 
+        };
+    } 
+    );
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +35,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
