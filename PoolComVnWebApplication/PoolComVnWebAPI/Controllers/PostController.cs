dcccb3fd30d41ac8a38cea258157547e025a1eee
dinhthenam <111273152+dinhthenam@users.noreply.github.com>
2024-02-21 22:04:49 +0700

    PostAPI
    CRUD và đã kiểm tra điều kiện. chưa authorized và tích hợp cketditor

---
+++ PoolComVnWebApplication/PoolComVnWebAPI/Controllers/PostController.cs
@@ -0,0 +1,97 @@
+﻿using BusinessObject.Models;
+using DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PoolComVnWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly PostDAO _newsDAO;
+
+        public PostController(PostDAO newsDAO)
+        {
+            _newsDAO = newsDAO;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<News>> Get()
+        {
+            var allNews = _newsDAO.GetAllNews();
+            return Ok(allNews);
+        }
+
+        // GET: api/News/5
+        [HttpGet("{id}")]
+        public ActionResult<News> Get(int id)
+        {
+            try
+            {
+                var news = _newsDAO.GetNewsById(id);
+
+                if (news == null)
+                {
+                    return NotFound(); 
+                }
+
+                return Ok(news);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+     
+        [HttpPost]
+        public ActionResult Post([FromBody] News news)
+        {
+            try
+            {
+                _newsDAO.AddNews(news);
+                return CreatedAtAction(nameof(Get), new { id = news.NewsID }, news);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message); 
+            }
+        }
+
+      
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] News updatedNews)
+        {
+            try
+            {
+                if (id != updatedNews.NewsID)
+                {
+                    return BadRequest("Invalid News ID"); 
+                }
+
+                _newsDAO.UpdateNews(updatedNews);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message); 
+            }
+        }
+
+        
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                _newsDAO.DeleteNews(id);
+                return NoContent(); 
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message); 
+            }
+        }
+    }
+}
