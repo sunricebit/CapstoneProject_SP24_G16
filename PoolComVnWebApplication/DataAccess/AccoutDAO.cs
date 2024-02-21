84735fa249615055fb2a2e2bdd87c76d9737e2e6
sunricebit <vuducduynb2002@gmail.com>
2024-02-21 11:22:24 +0700

    Add token manager and authen

---
+++ PoolComVnWebApplication/DataAccess/AccoutDAO.cs
@@ -0,0 +1,26 @@
+﻿namespace DataAccess
+{
+    public class AccoutDAO
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public void BanAccount() { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void RegisterAccount() { 
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void GetAllAccount() { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void AuthenAccount() { }
+    }
+}
