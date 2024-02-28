f170ddbb2331cee336b72d6ba033027a729b8512
vhnam22 <113097282+vhnam22@users.noreply.github.com>
2024-02-28 13:18:12 +0700

    NamVu_FE_Add tournamentDetails.html and tournamentBracket.html

---
+++ PoolComVnWebApplication/PoolComVnWebClient/Controllers/TournamentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PoolComVnWebClient.Controllers
+{
+    public class TournamentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult TournamentBracket()
+        {
+            return View();
+        }
+    }
+}
