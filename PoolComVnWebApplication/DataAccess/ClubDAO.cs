60e38ea5024b30a4cff34efaf3a00ae38fb0364a
Nguyễn Phi Long <98214053+CodeDebut@users.noreply.github.com>
2024-03-01 21:39:09 +0700

    CRUD API Club
    Tao, Doc, Lay theo ID, Cap nhat, Xoa profile CLUB

---
+++ PoolComVnWebApplication/DataAccess/ClubDAO.cs
@@ -0,0 +1,64 @@
+﻿using BusinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataAccess
+{
+    public class ClubDAO
+    {
+        private readonly PoolComContext _context;
+
+        public ClubDAO(PoolComContext poolComContext)
+        {
+            _context = poolComContext;
+        }
+
+        // Create
+        public void AddClub(Club club)
+        {
+            _context.Clubs.Add(club);
+            _context.SaveChanges();
+        }
+
+        // Read
+        public Club GetClubById(int clubId)
+        {
+            return _context.Clubs.Find(clubId);
+        }
+
+        public List<Club> GetAllClubs()
+        {
+            return _context.Clubs.ToList();
+        }
+
+        // Update
+        public void UpdateClub(Club updatedClub)
+        {
+            var existingClub = _context.Clubs.Find(updatedClub.ClubId);
+
+            if (existingClub != null)
+            {
+                existingClub.ClubName = updatedClub.ClubName;
+                existingClub.Address = updatedClub.Address;
+                existingClub.Phone = updatedClub.Phone;
+                existingClub.Facebook = updatedClub.Facebook;
+                existingClub.Avatar = updatedClub.Avatar;
+
+                _context.SaveChanges();
+            }
+        }
+
+        // Delete
+        public void DeleteClub(int clubId)
+        {
+            var clubToDelete = _context.Clubs.Find(clubId);
+
+            if (clubToDelete != null)
+            {
+                _context.Clubs.Remove(clubToDelete);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
--- PoolComVnWebApplication/PoolComVnWebAPI/Controllers/ClubController.cs
+++ PoolComVnWebApplication/PoolComVnWebAPI/Controllers/ClubController.cs
@@ -1,12 +1,100 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using BusinessObject.Models;
+using DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using PoolComVnWebAPI.DTO;
+using System.Collections.Generic;
 
 namespace PoolComVnWebAPI.Controllers
 {
-    public class ClubController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClubController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly ClubDAO _clubDAO;
+        private readonly IMapper _mapper;
+
+        public ClubController(ClubDAO clubDAO, IMapper mapper)
         {
-            return View();
+            _clubDAO = clubDAO;
+            _mapper = mapper;
+        }
+
+        // GET: api/Club
+        [HttpGet]
+        public ActionResult<IEnumerable<ClubDTO>> Get()
+        {
+            var clubs = _clubDAO.GetAllClubs();
+            var clubsDto = _mapper.Map<List<ClubDTO>>(clubs);
+            return Ok(clubsDto);
+        }
+
+        // GET: api/Club/5
+        [HttpGet("{id}")]
+        public ActionResult<ClubDTO> Get(int id)
+        {
+            var club = _clubDAO.GetClubById(id);
+
+            if (club == null)
+            {
+                return NotFound();
+            }
+
+            var clubDto = _mapper.Map<ClubDTO>(club);
+            return Ok(clubDto);
+        }
+
+        // POST: api/Club
+        [HttpPost]
+        public IActionResult Post([FromBody] ClubDTO clubDto)
+        {
+            if (clubDto == null)
+            {
+                return BadRequest();
+            }
+
+            var club = _mapper.Map<Club>(clubDto);
+            _clubDAO.AddClub(club);
+
+            return CreatedAtAction(nameof(Get), new { id = club.ClubId }, clubDto);
+        }
+
+        // PUT: api/Club/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] ClubDTO updatedClubDto)
+        {
+            if (updatedClubDto == null || id != updatedClubDto.ClubId)
+            {
+                return BadRequest();
+            }
+
+            var existingClub = _clubDAO.GetClubById(id);
+
+            if (existingClub == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(updatedClubDto, existingClub); // Update the existing entity
+            _clubDAO.UpdateClub(existingClub);
+
+            return NoContent();
+        }
+
+        // DELETE: api/Club/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var club = _clubDAO.GetClubById(id);
+
+            if (club == null)
+            {
+                return NotFound();
+            }
+
+            _clubDAO.DeleteClub(id);
+
+            return NoContent();
         }
     }
 }
