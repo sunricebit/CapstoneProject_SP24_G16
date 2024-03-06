315cc44dd3fb1720d08c9e7e30a93aef80ecf280
vhnam22 <113097282+vhnam22@users.noreply.github.com>
2024-03-06 17:36:55 +0700

    NamVu_FE_Add step 1,2 of create tour NamVu_FE_Modify tournamentDetails.html

---
+++ PoolComVnWebApplication/PoolComVnWebClient/Controllers/CreateTournamentController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PoolComVnWebClient.Controllers
+{
+    public class CreateTournamentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult StepOneCreateTournament()
+        {
+            return View();
+        }
+
+        public IActionResult StepTwoPlayerList()
+        {
+            return View();
+        }
+
+        public IActionResult StepTwoJoinList()
+        {
+            return View();
+        }
+
+        public IActionResult StepTwoMember()
+        {
+            return View();
+        }
+
+        public IActionResult StepTwoPlayerSystem()
+        {
+            return View();
+        }
+    }
+}
