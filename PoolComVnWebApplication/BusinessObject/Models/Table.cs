aec84a487a374d0e6e1468d8bc1d309b202f6412
dinhthenam <111273152+dinhthenam@users.noreply.github.com>
2024-03-09 01:45:26 +0700

    sửa lại database và update news ( đang lỗi phần up ảnh lên database)

--- PoolComVnWebApplication/BusinessObject/Models/Table.cs
+++ PoolComVnWebApplication/BusinessObject/Models/Table.cs
@@ -5,10 +5,20 @@ namespace BusinessObject.Models
 {
     public partial class Table
     {
+        public Table()
+        {
+            MatchOfTournaments = new HashSet<MatchOfTournament>();
+        }
+
         public int TableId { get; set; }
         public string TableName { get; set; } = null!;
         public int ClubId { get; set; }
+        public string TagName { get; set; } = null!;
+        public bool Status { get; set; }
+        public string Size { get; set; } = null!;
+        public string Image { get; set; } = null!;
 
         public virtual Club Club { get; set; } = null!;
+        public virtual ICollection<MatchOfTournament> MatchOfTournaments { get; set; }
     }
 }
--- PoolComVnWebApplication/BusinessObject/Models/Tournament.cs
+++ PoolComVnWebApplication/BusinessObject/Models/Tournament.cs
@@ -24,16 +24,16 @@ namespace BusinessObject.Models
         public string RaceToString { get; set; } = null!;
         public string PaymentType { get; set; } = null!;
         public DateTime RegistrationDeadline { get; set; }
-        public int AccessId { get; set; }
         public int MaxPlayerNumber { get; set; }
         public byte Status { get; set; }
         public int ClubId { get; set; }
-        public int DrawTypeId { get; set; }
+        public bool? Accesses { get; set; }
+        public string? Flyer { get; set; }
+        public int? PlayerTypeId { get; set; }
 
-        public virtual Access Access { get; set; } = null!;
         public virtual Club Club { get; set; } = null!;
-        public virtual DrawType DrawType { get; set; } = null!;
         public virtual GameType GameType { get; set; } = null!;
+        public virtual PlayerType? PlayerType { get; set; }
         public virtual TournamentType TournamentType { get; set; } = null!;
         public virtual ICollection<MatchOfTournament> MatchOfTournaments { get; set; }
         public virtual ICollection<Player> Players { get; set; }
