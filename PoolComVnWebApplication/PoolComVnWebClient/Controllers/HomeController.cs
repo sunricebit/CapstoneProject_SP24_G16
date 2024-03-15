9364dd43c9548fe7551e3f80724f67fee5a430e8
Nguyễn Phi Long <98214053+CodeDebut@users.noreply.github.com>
2024-03-15 23:18:13 +0700

    Đẩy New mới nhất lên homepage
    Đẩy New mới nhất lên homepage, viết thêm api GetLastestNews, dùng Annotations để check input login

--- PoolComVnWebApplication/PoolComVnWebClient/Controllers/HomeController.cs
+++ PoolComVnWebApplication/PoolComVnWebClient/Controllers/HomeController.cs
@@ -1,25 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PoolComVnWebClient.Common;
+using PoolComVnWebClient.DTO;
 using PoolComVnWebClient.Models;
 using System.Diagnostics;
+using System.Net.Http.Headers;
 
 namespace PoolComVnWebClient.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        private readonly HttpClient client = null;
+        private string ApiUrl = Constant.ApiUrl;
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+            ApiUrl = ApiUrl + "/News";
         }
 
         [HttpGet]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            try
+            {
+                var response = await client.GetAsync($"{ApiUrl}/GetLatestNews?count=6"); // Lấy 6 tin tức mới nhất
+                response.EnsureSuccessStatusCode(); // Đảm bảo phản hồi thành công
+
+                var jsonContent = await response.Content.ReadAsStringAsync();
+                var newsList = JsonConvert.DeserializeObject<List<NewsDTO>>(jsonContent);
+                return View(newsList);
+            }
+            catch (HttpRequestException ex)
+            {
+                // Xử lý lỗi nếu không thể kết nối đến API
+                ModelState.AddModelError(string.Empty, "Lỗi khi kết nối đến API: " + ex.Message);
+                return View();
+            }
+            catch (Exception ex)
+            {
+                // Xử lý các lỗi khác
+                ModelState.AddModelError(string.Empty, "Lỗi khi lấy danh sách tin tức: " + ex.Message);
+                return View();
+            }
         }
 
-        [HttpGet("NewsDetails")]
+
+        [HttpGet]
         public IActionResult NewsDetail()
         {
             return View();
