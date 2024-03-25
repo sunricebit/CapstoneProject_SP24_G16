92cb5711dbd788f861b0fd8812f42dcba8f6279f
dinhthenam <111273152+dinhthenam@users.noreply.github.com>
2024-03-26 00:37:20 +0700

    sửa db bảng matchoftournaments

--- PoolComVnWebApplication/BusinessObject/Models/poolcomvnContext.cs
+++ PoolComVnWebApplication/BusinessObject/Models/poolcomvnContext.cs
@@ -43,8 +43,8 @@ namespace BusinessObject.Models
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var builder = new ConfigurationBuilder()
-                                              .SetBasePath(Directory.GetCurrentDirectory())
-                                              .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                                             .SetBasePath(Directory.GetCurrentDirectory())
+                                             .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
             IConfigurationRoot configuration = builder.Build();
             optionsBuilder.UseSqlServer(configuration.GetConnectionString("PoolCom"));
         }
